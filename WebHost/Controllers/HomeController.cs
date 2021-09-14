f8f0a3cdeb145374b9abdbe1173b4620eb4cf39d
limingbo2019 <56576416+limingbo2019@users.noreply.github.com>
2021-09-14 17:28:44 +0800

    提交第一个模板
    模板最重要的就是配置，同级目录的才能放在目录中

---
+++ WebHost/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+       public Task Get()
+        {
+            return Redirect("");
+        }
+    }
+}
